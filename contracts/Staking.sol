fc3d4700166e924ee2c25fe0e0cd1ee5b6665528
mikbolshakov <107908985+mikbolshakov@users.noreply.github.com>
2022-12-07 22:08:29 +0300

    dev contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,79 +1,77 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract Staking {
-    IERC20 rewardsToken;
-    IERC20 stakingToken;
+    IERC20 public token;
+    uint public totalPoolAmount; 
+    uint rewardRatePerMinute;
 
-    uint public TVL; // tvl стейкинг пула
-    uint rewardRate = 10;
-    uint reward;
-    uint lastUpdateTime;
-    uint rewardPerTokenStored;
-    uint lockTime; // lock time in minutes
-    uint onStaking;
+    mapping(address => uint) public stakingAmount; // address => staking amount of this address
+    mapping(address => uint) private lockTime; // address => number of minutes in staking
+    mapping(address => uint) private endTimeOfStaking; // address => end time of staking
+    mapping(address => uint) public rewardPerMinute; // address => reward amount in one minute
 
-    mapping(address => uint) userRewardPerTokenPaid;
-    mapping(address => uint) public rewards;
-    mapping(address => uint) public balances;
-    mapping(address => uint) private onStaking;
+    event NewDepositor(address sender, uint amount, uint startsAt, uint endsAt);
 
-    modifier updateReward(address _account) {
-        rewardPerTokenStored = rewardPerToken();
-        lastUpdateTime = block.timestamp;
-        rewards[_account] = earned(_account);
-        userRewardPerTokenPaid[_account] = rewardPerTokenStored;
-        _;
+    constructor(uint _rewardRatePerMinute, IERC20 _token) {
+        token = _token;
+        rewardRatePerMinute = _rewardRatePerMinute;
     }
 
-    // разместить монеты на стейкинг (сумма, срок в минутах)
     function putInStaking(uint _amount, uint _lockTime) public payable {
-        onStaking[msg.sender] = _amount;
-        lockTime = block.timestamp + _lockTime * 60;
-        TVL += _amount;
-        reward = _amount * rewardRate / 100;
-        stakingToken.transferFrom(msg.sender, balance(this), _amount);
+        require(stakingAmount[msg.sender] == 0, "already in staking");
+        totalPoolAmount += _amount;
+        stakingAmount[msg.sender] = _amount;
+        lockTime[msg.sender] = _lockTime;
+        endTimeOfStaking[msg.sender] = block.timestamp + _lockTime * 60;
+        rewardPerMinute[msg.sender] = _amount * rewardRatePerMinute / 100;
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        emit NewDepositor(msg.sender, _amount, block.timestamp, lockTime[msg.sender]);
     }
 
     // вывести из стейкинга по окончанию срока
     function pullOutOfStaking() public {
-        require(block.timestamp >= lockTime);
-        TVL -= onStaking[msg.sender];
-        onStaking[msg.sender] = 0;
-        uint pullOut = onStaking[msg.sender] + reward
-        stakingToken.transfer(msg.sender, pullOut);
+        require(block.timestamp >= endTimeOfStaking[msg.sender], "Staking time is not over");
+        uint pullOut = stakingAmount[msg.sender] + rewardPerMinute[msg.sender] * lockTime[msg.sender];
+        totalPoolAmount -= stakingAmount[msg.sender];
+        stakingAmount[msg.sender] = 0;
+        endTimeOfStaking[msg.sender] = 0;
+        rewardPerMinute[msg.sender] = 0;
+        lockTime[msg.sender] = 0;
+        token.transfer(msg.sender, pullOut);
     }
 
-    // function getReward() external updateReward(msg.sender) {
-    //     uint reward = rewards[msg.sender];
-    //     rewards[msg.sender] = 0;
-    //     rewardsToken.transfer(msg.sender, reward);
-    // }
+    fallback() external payable {}
 
-    // function rewardPerToken() public view returns(uint) {
-    //     if(totalSupply == 0) {
-    //         return 0;
-    //     }
-
-    //     return rewardPerTokenStored + (
-    //         rewardRate * (block.timestamp - lastUpdateTime)
-    //         ) * 1e18 / totalSupply;
-    // }
-
-    // function earned(address _account) public view returns(uint) {
-    //     return (
-    //         balances[_account] * (
-    //         rewardPerToken() - userRewardPerTokenPaid[_account]
-    //         ) / 1e18
-    //     ) + rewards[_account];
-    // }
-    
-
-    // вывести из стейкинга по окончанию срока
-    // проценты в конце срока при выводе из стейкинга 
-
-    // список вкладчиков (адрес, сумма, дата начала и окончания стейкинга)
-
-
-    // настройки: % начисления в минуту, тариф (3, 5, 10 мин), адрес владельца для начисления процентов, сумма комиссии
+    receive() external payable {}
 }
+
+/*
+Написать смарт-контракт в сети Tron для стейкинга TRX.
+
+Смарт контракт должен содержать следующие методы:
++ Разместить монеты на стейкинг (сумма, срок в минутах)
++ Освобождение монет из стейкинга по окончании срока
+-+ Вывести список вкладчиков (кошельки, суммы, дата вклада, дата завершения сетйкинга)
+    // вывел на фронте через событие
++ Вывести общий баланс в стейкинге
+
+Смарт контракт должен содержать блок настроек:
+- процент начислений за стейкинг из расчета % / минуту,
+-+ список тарифов по времени (3 минуты, 5 минут, 10 минут), 
+    // я же правильно понял - суть в том, чтобы дублировать фнукции со строгим значением _lockTime
+- адрес кошелька владельца для начисления процентов, 
+- сумма комиссии
+Начисление процентов осуществляется в конце срока депозита при выводе пользователю на
+кошелек
+
+Опубликовать в тестнете сети tron, верифицировать.. Код покрыть тестами.
+Для проверки и оценки результата необходимо предоставить:
+- Адрес контракта в сети
+- Ссылку на GIT репозиторий для проверки кода
+- Описание структурной схемы проекта (xmind)
+- Описание работы и методов подключения
+*/
